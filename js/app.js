c54cadea69b00a2aee27fe97c834eef2d02bf157
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-06-22 16:41:16 -0400

    Zombies now move randomly on there own and stay on the game map.

--- js/app.js
+++ js/app.js
@@ -29,16 +29,53 @@ class Sprite {
         this.height = height
         
         this.alive = true,
+        this.shiftyness = 10
+        this.shiftynessTimer = 0
+        
         //methods
         this.render = function () {
             ctx.fillStyle = this.color
             ctx.fillRect(this.x, this.y, this.width, this.height)
         }
+        
         this.changeDirection = function(direction) {
             this.direction = direction
+        }
+        
+        this.move = function() {
+            //if change direction
+            this.shiftynessTimer++
+            if (this.shiftynessTimer === this.shiftyness) {//////////
+                p('changing direction.')
+                this.shiftynessTimer = 0
+                this.direction = randomDirectionChange(this)}
+
+            p(this.direction)
+            //if direction
+            switch (this.direction) {
+                case ('up'):
+                    if (bounderies(this)){
+                        this.y -= playerSpeed
+                }
+                break;
+                case ('left'):
+                    if (bounderies(this)){
+                        this.x -= playerSpeed
+                }   
+                break;
+                case ('down'):
+                    if (bounderies(this)){
+                        this.y += playerSpeed
+                }   
+                break;
+                case ('right'):
+                    if (bounderies(this)){
+                        this.x += playerSpeed
+                }   
+                    break;
+            }    
         } 
     }
-
 }
 
 //Make players on the board.
@@ -50,10 +87,10 @@ for (let i = 0; i < 1; i++){
     //     Math.floor(Math.random() * (game.height - (spriteHeight + (10 * 2)))) + 10, 
     //     'up', '#bada55', spriteWidth, spriteHeight))
     zombie.push(new Sprite(
-        (game.width/6), 
+        (game.width/2), 
         (game.height/2), 
         'right', '#bada55', spriteWidth, spriteHeight))
-} //TODO NOTE need check if face each other player can't knife.
+} 
 
 // MAIN FUNCTION \\
 const gameLoop = () => {
@@ -71,10 +108,10 @@ const gameLoop = () => {
     zombie.forEach(zombie => {
         if (zombie.alive === true) {
             zombie.render()
+            zombie.move()
         }
     })
 }
-
 
 // we're going to do this, when the content loads
 document.addEventListener('DOMContentLoaded', function () {
@@ -140,7 +177,7 @@ const movementHandler = (e) => {
     switch (e.key) {
         case ('w'):
         case ('ArrowUp'):
-            if (bounderies(e.key)){
+            if (bounderies(player)){
                 player.changeDirection('up')
                 player.y -= playerSpeed
             }    
@@ -149,14 +186,14 @@ const movementHandler = (e) => {
         case ('ArrowLeft'):
         // case (40):
             // this moves the player left
-            if (bounderies(e.key)){
+            if (bounderies(player)){
                 player.changeDirection('left')
                 player.x -= playerSpeed
             }
             break
         case ('s'):
         case ('ArrowDown'):
-            if (bounderies(e.key)){
+            if (bounderies(player)){
                 player.changeDirection('down')
                 player.y += playerSpeed
             }
@@ -164,7 +201,7 @@ const movementHandler = (e) => {
         case ('d'):
         case ('ArrowRight'):
             // this moves the player to the right
-            if (bounderies(e.key)){
+            if (bounderies(player)){
                 player.changeDirection('right')
                 player.x += playerSpeed
             }
@@ -188,22 +225,22 @@ const detectHit = () => {
     })
 }
 const killZombie = zombie => {
-    // zombie.alive = false
-    // killCount++
+    zombie.alive = false
+    killCount++
     p('KILLED!!!!!!') 
 } 
 // Will think about adding asdw keys.
-const bounderies = (key) => {
+const bounderies = (sprite) => {
     // console.log(`Player.x: ${player.x} game.width: ${game.width}`)
-    if (key === 'ArrowLeft' && player.x - playerSpeed < 0){
+    if (sprite.direction === 'left' && sprite.x - playerSpeed < 0){
         // console.log(player.x + " game.width"+game.width)
         return false 
     }    
-    if (key === 'ArrowRight' && player.x + playerSpeed + player.width > game.width)
+    if (sprite.direction === 'right' && sprite.x + playerSpeed + player.width > game.width)
         return false
-    if (key === 'ArrowUp' && player.y - playerSpeed < 0)
+    if (sprite.direction === 'up' && sprite.y - playerSpeed < 0)
         return false
-    if (key === 'ArrowDown' && player.y + playerSpeed + player.height > game.height)
+    if (sprite.direction === 'down' && sprite.y + playerSpeed + player.height > game.height)
         return false
     return true
 }
@@ -217,3 +254,25 @@ const convertDirectionNumber = (sprite) => {
     if (sprite.direction === 'left')
         return 3
 } 
+const randomDirectionChange = (sprite) => {
+    p("Inside function")
+    const leftOrRight = Math.round(Math.random())
+    p("leftOrRigth: " + leftOrRight)
+    let newDirectionInt = null
+    if (leftOrRight === 0) {
+        newDirectionInt = convertDirectionNumber(sprite) + 3 //no - num
+    } else { 
+        newDirectionInt = convertDirectionNumber(sprite) + 1}
+    p("newDirection in Int: "+newDirectionInt)
+    newDirectionInt %= 4
+    p("NDirection %4: "+newDirectionInt)
+
+    if (newDirectionInt === 0)
+        return 'up'
+    if (newDirectionInt === 1)
+        return 'right'
+    if (newDirectionInt === 2)
+        return 'down'
+    if (newDirectionInt === 3)
+        return 'left'
+}
