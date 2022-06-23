f2151512a97823938b3c8627a10043a3ded12e56
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-06-22 23:50:20 -0400

    changed to pass object and not return them but change the object. upgraded the bondery detection

--- js/app.js
+++ js/app.js
@@ -2,10 +2,11 @@ const p = (str) => {console.log(str)}
 const game = document.getElementById('canvas')
 const messageBoard = document.getElementById('movement')
 let killCount = 0
-const playerSpeed = 8
 const spriteHeight = 25
 const spriteWidth = 20
-const swingRange = spriteHeight + 10
+const playerSpeed = 8
+const zombieSpeed = 3
+// const swingRange = spriteHeight + 10 //That is a huge sword.
 const knifeRange = 20
 
 // we also need to define our game context
@@ -20,10 +21,11 @@ console.log('this is the canvas height', game.height)
 
 // Objects are made of properties(K:v pairs) and methods(functions)
 class Sprite {
-    constructor(x, y, direction, color, width, height) {
+    constructor(x, y, direction, speed, color, width, height) {
         this.x = x,
         this.y = y,
         this.direction = direction
+        this.speed = speed
         this.color = color,
         this.width = width,
         this.height = height
@@ -33,11 +35,6 @@ class Sprite {
         this.shiftynessTimer = 0
         
         //methods
-        this.render = function () {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this.width, this.height)
-        }
-        
         this.changeDirection = function(direction) {
             this.direction = direction
         }
@@ -45,41 +42,50 @@ class Sprite {
         this.move = function() {
             //if change direction
             this.shiftynessTimer++
-            if (this.shiftynessTimer === this.shiftyness) {//////////
+            // p(`shiftynessTimer: ${this.shiftynessTimer}`)
+            if (this.shiftynessTimer === this.shiftyness) {
                 p('changing direction.')
                 this.shiftynessTimer = 0
-                this.direction = randomDirectionChange(this)}
-
-            p(this.direction)
+                randomDirectionChange(this)}
+                
+            // p(this.direction)
             //if direction
             switch (this.direction) {
                 case ('up'):
-                    if (bounderies(this)){
-                        this.y -= playerSpeed
-                }
+                    this.y -= this.speed
+                    if (this.y <= 0) { //Need ask about = in <=
+                        this.y = 0
+                        this.direction = 'down'}
                 break;
                 case ('left'):
-                    if (bounderies(this)){
-                        this.x -= playerSpeed
-                }   
+                    this.x -= this.speed
+                    if (this.x <= 0 ){
+                        this.x = 0
+                        this.direction = 'right'}
                 break;
                 case ('down'):
-                    if (bounderies(this)){
-                        this.y += playerSpeed
-                }   
+                    this.y += this.speed
+                    if (this.y >= game.height){
+                        this.y = game.height
+                        this.direction = 'up'}
                 break;
                 case ('right'):
-                    if (bounderies(this)){
-                        this.x += playerSpeed
-                }   
-                    break;
+                    this.x += this.speed
+                    if (this.x >= game.width){
+                        this.x = game.width
+                        this.direction = 'left'}   
+                break;
             }    
         } 
+        this.render = function () {
+            ctx.fillStyle = this.color
+            ctx.fillRect(this.x, this.y, this.width, this.height)
+        }
     }
 }
 
 //Make players on the board.
-let player = new Sprite(100, game.height/2, 'up', 'lightsteelblue', spriteWidth, spriteHeight)
+let player = new Sprite(100, game.height/4, 'up', playerSpeed, 'lightsteelblue', spriteWidth, spriteHeight)
 let zombie = []
 for (let i = 0; i < 1; i++){
     // zombie.push(new Sprite(
@@ -89,7 +95,7 @@ for (let i = 0; i < 1; i++){
     zombie.push(new Sprite(
         (game.width/2), 
         (game.height/2), 
-        'right', '#bada55', spriteWidth, spriteHeight))
+        'right', zombieSpeed, '#bada55', spriteWidth, spriteHeight))
 } 
 
 // MAIN FUNCTION \\
@@ -97,8 +103,10 @@ const gameLoop = () => {
     
     ctx.clearRect(0, 0, game.width, game.height)
     
-    messageBoard.textContent = player.x + ', ' + player.y + "\nKills: " + killCount
+    messageBoard.textContent = player.x + ', ' + player.y + '\nKills: ' + killCount 
     
+    // const str = "Zombie X: "+zombie[0].x+"\nZombe Y: "+zombie[0].y
+    // p(str)
     detectHit()
 
     if (player.alive === true) {
@@ -177,39 +185,40 @@ const movementHandler = (e) => {
     switch (e.key) {
         case ('w'):
         case ('ArrowUp'):
-            if (bounderies(player)){
-                player.changeDirection('up')
-                player.y -= playerSpeed
-            }    
+            player.changeDirection('up')
+            player.y -= player.speed
+            if (player.y <= 0){
+                player.y = 0}    
             break
         case ('a'):
         case ('ArrowLeft'):
         // case (40):
             // this moves the player left
-            if (bounderies(player)){
-                player.changeDirection('left')
-                player.x -= playerSpeed
-            }
-            break
+            player.changeDirection('left')
+                player.x -= player.speed
+            if (player.x < 0){
+                player.x = 0}
+        break
         case ('s'):
         case ('ArrowDown'):
-            if (bounderies(player)){
-                player.changeDirection('down')
-                player.y += playerSpeed
-            }
-            break
+            player.changeDirection('down')
+            player.y += player.speed
+            if (player.y > game.height)
+                player.y = game.height
+        break
         case ('d'):
         case ('ArrowRight'):
             // this moves the player to the right
-            if (bounderies(player)){
-                player.changeDirection('right')
-                player.x += playerSpeed
+            player.changeDirection('right')
+            player.x += player.speed
+            if (player.x > game.width){
+                player.x = game.width                
             }
-            break
+        break
         case (' '): //spacebar
             // console.log("sing!!!!")
             knifeSwing()
-            break
+        break
     }
 }
 const detectHit = () => {
@@ -229,21 +238,6 @@ const killZombie = zombie => {
     killCount++
     p('KILLED!!!!!!') 
 } 
-// Will think about adding asdw keys.
-const bounderies = (sprite) => {
-    // console.log(`Player.x: ${player.x} game.width: ${game.width}`)
-    if (sprite.direction === 'left' && sprite.x - playerSpeed < 0){
-        // console.log(player.x + " game.width"+game.width)
-        return false 
-    }    
-    if (sprite.direction === 'right' && sprite.x + playerSpeed + player.width > game.width)
-        return false
-    if (sprite.direction === 'up' && sprite.y - playerSpeed < 0)
-        return false
-    if (sprite.direction === 'down' && sprite.y + playerSpeed + player.height > game.height)
-        return false
-    return true
-}
 const convertDirectionNumber = (sprite) => {
     if (sprite.direction === 'up')
         return 0
@@ -255,24 +249,24 @@ const convertDirectionNumber = (sprite) => {
         return 3
 } 
 const randomDirectionChange = (sprite) => {
-    p("Inside function")
+    // p("Inside function")
     const leftOrRight = Math.round(Math.random())
-    p("leftOrRigth: " + leftOrRight)
+    // p("leftOrRigth: " + leftOrRight)
     let newDirectionInt = null
     if (leftOrRight === 0) {
         newDirectionInt = convertDirectionNumber(sprite) + 3 //no - num
     } else { 
         newDirectionInt = convertDirectionNumber(sprite) + 1}
-    p("newDirection in Int: "+newDirectionInt)
+    // p("newDirection in Int: "+newDirectionInt)
     newDirectionInt %= 4
-    p("NDirection %4: "+newDirectionInt)
+    // p("NDirection %4: "+newDirectionInt)
 
-    if (newDirectionInt === 0)
-        return 'up'
-    if (newDirectionInt === 1)
-        return 'right'
-    if (newDirectionInt === 2)
-        return 'down'
-    if (newDirectionInt === 3)
-        return 'left'
+    if (newDirectionInt === 0){
+        sprite.direction = 'up'}
+    if (newDirectionInt === 1){
+        sprite.direction = 'right'}
+    if (newDirectionInt === 2){
+        sprite.direction = 'down'}
+    if (newDirectionInt === 3){
+        sprite.direction = 'left'}
 }
